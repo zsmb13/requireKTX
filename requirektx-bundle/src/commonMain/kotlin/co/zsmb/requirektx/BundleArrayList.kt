c6d0b579598348296f6677ac6458076a4e4ee8b0
Márton Braun <braunmarci@gmail.com>
2024-05-18 02:59:39 +0200

    Fix comment indentation

--- requirektx-bundle/src/commonMain/kotlin/co/zsmb/requirektx/BundleArrayList.kt
+++ requirektx-bundle/src/commonMain/kotlin/co/zsmb/requirektx/BundleArrayList.kt
@@ -28,8 +28,8 @@ public inline fun Bundle.requireCharSequenceArrayList(key: String): ArrayList<Ch
 
 /**
  * Returns the value associated with the given key, or null if the key doesn't exist,
-* or the stored value is of the wrong type.
-*/
+ * or the stored value is of the wrong type.
+ */
 public inline fun Bundle.getCharSequenceArrayListOrNull(key: String): ArrayList<CharSequence>? = getOrNullImpl(key)
 
 /**
