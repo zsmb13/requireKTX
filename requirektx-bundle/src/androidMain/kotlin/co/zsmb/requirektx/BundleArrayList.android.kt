ebc7b6c850d08c138abd341efffd253e211ef85b
Márton Braun <braunmarci@gmail.com>
2024-05-18 02:58:10 +0200

    Move getCharSequenceArrayListOrNull back to common

--- requirektx-bundle/src/androidMain/kotlin/co/zsmb/requirektx/BundleArrayList.android.kt
+++ requirektx-bundle/src/androidMain/kotlin/co/zsmb/requirektx/BundleArrayList.android.kt
@@ -6,21 +6,15 @@ import androidx.core.bundle.Bundle
 import android.os.Parcelable
 
 /**
-    * Returns the value associated with the given key.
-    *
-    * @throws IllegalArgumentException if the key does not exist.
-    * @throws IllegalStateException if the stored value is of the wrong type.
-    */
+ * Returns the value associated with the given key.
+ *
+ * @throws IllegalArgumentException if the key does not exist.
+ * @throws IllegalStateException if the stored value is of the wrong type.
+ */
 public inline fun Bundle.requireParcelableArrayList(key: String): ArrayList<Parcelable> = requireImpl(key)
 
 /**
-    * Returns the value associated with the given key, or null if the key doesn't exist,
-    * or the stored value is of the wrong type.
-    */
-public inline fun Bundle.getParcelableArrayListOrNull(key: String): ArrayList<Parcelable>? = getOrNullImpl(key)
-
-/**
  * Returns the value associated with the given key, or null if the key doesn't exist,
-* or the stored value is of the wrong type.
-*/
-public inline fun Bundle.getCharSequenceArrayListOrNull(key: String): ArrayList<CharSequence>? = getOrNullImpl(key)
+ * or the stored value is of the wrong type.
+ */
+public inline fun Bundle.getParcelableArrayListOrNull(key: String): ArrayList<Parcelable>? = getOrNullImpl(key)
